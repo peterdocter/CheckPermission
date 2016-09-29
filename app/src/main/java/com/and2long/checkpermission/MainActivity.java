e3bf33886dcb3a9cfa753dc7914af60a748fce81
and2long <and2long@gmail.com>
2016-09-29 14:44:12 +0800

    修改授权逻辑

--- app/src/main/java/com/and2long/checkpermission/MainActivity.java
+++ app/src/main/java/com/and2long/checkpermission/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.callPhone:
                 //检查权限
-                checkPermissionsAndDoNext(Manifest.permission.CALL_PHONE, MY_PERMISSIONS_REQUEST);
+                checkPermissionsAndDoNext();
                 break;
         }
     }
@@ -48,29 +48,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     /**
      * 检查是否具有权限
      *
-     * @param permission
-     * @return
      */
-    public void checkPermissionsAndDoNext(String permission, int request) {
-        if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this,
-                permission)) {
-            //具有权限，执行操作
-            call();
-        } else {
-            //没有权限
+    public void checkPermissionsAndDoNext() {
+        if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CALL_PHONE)) {
             //如果app之前请求过该权限,被用户拒绝, 这个方法就会返回true.
             //如果用户之前拒绝权限的时候勾选了对话框中"Don’t ask again"的选项,那么这个方法会返回false.
             //如果设备策略禁止应用拥有这条权限, 这个方法也返回false.
             if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                    permission)) {
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-                ActivityCompat.requestPermissions(this,
-                        new String[]{permission},
-                        request);
-            } else {
+                    Manifest.permission.CALL_PHONE)) {
                 //提示用户需要权限
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setTitle(R.string.help)
@@ -82,10 +68,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                 dialog.dismiss();
                             }
                         })
-                        .setNeutralButton(R.string.setting, new DialogInterface.OnClickListener() {
+                        .setPositiveButton(R.string.setting, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                //进入设置，让用户手动授权
+                                //进入设置中的应用信息详情页，让用户手动授权
                                 Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
                                 intent.setData(Uri.parse("package:" + getPackageName()));
                                 startActivity(intent);
@@ -93,7 +79,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         })
                         .create()
                         .show();
+            } else {
+                //没有权限，请求权限。
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CALL_PHONE},
+                        MY_PERMISSIONS_REQUEST);
             }
+        } else {
+            //具有权限，执行操作
+            call();
         }
     }
 
@@ -104,11 +98,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    //已授权
+                    //授权
                     call();
-
                 } else {
-                    //没有授权
+                    //拒绝
 
                 }
                 break;
